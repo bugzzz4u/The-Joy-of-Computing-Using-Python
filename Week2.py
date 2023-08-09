c2979f17097f344112448022b8be4a8d5581f173
bugzzz4u <bhargavikoul@gmail.com>
2023-08-09 22:03:48 +0530

    Create Week2.py

---
+++ Week2.py
@@ -0,0 +1,19 @@
+t1="Amit"
+print(t1)
+t2="Simran"
+t3="Vidya"
+print("Hello t1")
+print("Hello", t1)
+print("Hello",t2)
+t1= "Amit_Verma"
+print(t1)
+age= 30
+print("THE NUMBER YOU ENTERED WAS", age)
+print(age)
+age = age + 1
+print(age)
+
+
+print("What is your name?")
+n=input("What is your name?")
+
